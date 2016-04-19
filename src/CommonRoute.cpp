6a5da83579a6a77adc17f7bdd55533a8a4743a26
Thakur Raj <ThakurRaj1151@gmail.com>
2016-04-19 20:38:53 +0530

    Submit commit

--- src/CommonRoute.cpp
+++ src/CommonRoute.cpp
@@ -49,7 +49,7 @@ First Island in DTD ie 'D' occurs alphabatically before 'H' and 'Z')
 #include <stdio.h>
 
 
-char * find_common_route(char * hacklist, char *codelist, int *cost){
+char * find_common_route(char * hacklist, char *codelist, int *cost) {
 	return NULL;
 }
 
